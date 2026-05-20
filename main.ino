bb082ec5bd99fc91c7f03272c903504675bdd12e
Nathaniel Gattis <68723896+GalaxyS29@users.noreply.github.com>
2026-05-20 16:52:45 -0500

    Clean up final main code
    Refactored several sections for clarity and removed deprecated code.

--- main.ino
+++ main.ino
@@ -1,10 +1,10 @@
 // data to be sent, guarantees 32 bytes total
-struct DataStruct {
+struct DataStruct
+{
     uint8_t C;              //  8
     uint8_t M;              //  8
     uint8_t Y;              //  8
     uint8_t K;              //  8
-    //byte padding[16];   // 24 Changes these data types to float and adjusted padding to 16t to account
                             //------
                             // 32
 };
@@ -19,7 +19,7 @@ bool newStepData = false;
 #include <string.h>
 #include <math.h>
 
-const byte thisAddress = 8; // these need to be swapped for the other Arduino
+const byte thisAddress = 8; // these need to be swapped for the slave Arduino
 const byte otherAddress = 9;
 
 // timing variables
@@ -52,6 +52,10 @@ char currHex[8] = {000000};
 
 LiquidCrystal lcd (13, 12, 11, 10, 9, 8);
 
+char hexAlphabet[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+int hexScrollCount = 0;
+int currHexPos = 0;
+
 // color sensor subsystem
 const int colorSensorPin0 = A4;
 const int colorSensorPin1 = A5;
@@ -62,23 +66,21 @@ uint16_t colorTemp, lux;
 float C, M, Y, K;
 uint16_t red, green, blue, clear;
 
-float rFact = 1450;    //1.9
-float gFact = 910;  //1.001
-float bFact = 650;    //0.7
+float rFact = 1450;
+float gFact = 910;
+float bFact = 650;
 
 int luxCount = 0;
-
-char hexAlphabet[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-int hexScrollCount = 0;
-int currHexPos = 0;
 
 uint32_t r, g, b;
 
 //_____________________________________________________________________________________________________________________________
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   Serial.println("Begin Setup...");
 
+  // check for RGB sensor
   if (!tcs.begin())
   {
     Serial.println("Sensor not found ... check your connections");
@@ -89,46 +91,33 @@ void setup() {
   }
 
   Serial.println("Sensor Found");
-
   Serial.println("Starting I2C Master Arduino...");
 
   // set up I2C
-  Wire.begin(thisAddress); // join i2c bus
+  Wire.begin(thisAddress);
 
-
+  // define pin modes for interface buttons to counter debounce
   pinMode(button0Pin, INPUT_PULLUP);
   pinMode(button1Pin, INPUT_PULLUP);
   pinMode(button2Pin, INPUT_PULLUP);
   pinMode(button3Pin, INPUT_PULLUP);
-  	
+
+  // initialize LCD screen
   lcd.begin(16,2);
-	lcd.clear();
+  lcd.clear();
   lcd.setCursor(0,0);
   sprintf(currHex, "%02X%02X%02X", 00, 00, 00);
 }
 
-
-
-
 //_____________________________________________________________________________________________________________________________
-void loop() {
-
-
-  /* This is for testing variables
-  Serial.print("C");
-  Serial.println(C); // Prints the sensor reading and a newline
-  delay(1000); // Wait for a second
-  C = C + 35;
-  */
-
+void loop()
+{
   // main switch-case function; each case accounts for its respective screen
-  switch(currScreen) {
+  switch(currScreen)
+  {
       case 0:
-        // HOME SCREEN
-      	//Serial.print("Home screen code start ");
-      	//Serial.println(currScreen);
-      
-		    lcd.setCursor(0,0);
+        // HOME SCREEN      
+		lcd.setCursor(0,0);
         lcd.print("#");
         lcd.setCursor(1,0);
       	lcd.print(currHex);
@@ -144,10 +133,7 @@ void loop() {
 
       case 1:
         // RGB SCANNING SCREEN
-      	//Serial.print("Scanning screen code start ");
-      	//Serial.println(currScreen);
-
-		    lcd.setCursor(0,0);
+		lcd.setCursor(0,0);
         lcd.print("#");
         lcd.setCursor(1,0);
       	lcd.print(currHex);
@@ -159,36 +145,20 @@ void loop() {
         break;
 
       case 2:
-        // !!!!!!DEPRECATED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!THIS IS NOT USED ANYMORE!!!!!!!!!
-        // HEXADECIMAL LOOKUP SCREEN
-      	//Serial.print("Hex lookup screen code start ");
-      	//Serial.println(currScreen);
-      
-		    //lcd.setCursor(0,0);
-      	//lcd.print(currHex);
-      
-      	//lcd.setCursor(0,1);
-      	//lcd.print("SCR SEL ENT BACK");
-      
+        // HEXADECIMAL LOOKUP SCREEN | DEPRECATED      
       	currScreen = getCurrScreen(2);
         break;
 
       case 3:
-        // HEXADECIMAL ENTRY SCREEN
-      	//Serial.print("Hex entry screen code start ");
-      	//Serial.println(currScreen);
-      
-		    lcd.setCursor(0,0);
+        // HEXADECIMAL ENTRY SCREEN | handles manual hex color entry and editing
+		lcd.setCursor(0,0);
       	lcd.print(currHex);
 
         lcd.setCursor(10,0);
       	lcd.print("POS:");
         lcd.setCursor(15, 0);
         lcd.print(currHexPos + 1);
-      	
-      	// there will be a enterCurrHex() function that will
-      	// handle manual entry of a new hexadecimal code
-      	
+      	      	
       	lcd.setCursor(0,1);
       	lcd.print("SCR NEX     BACK");
       
@@ -196,72 +166,43 @@ void loop() {
         break;
 
       case 4:
-        // MIXING SCREEN
-      	//Serial.print("Mixing setup screen code start ");
-      	//Serial.println(currScreen);
-      
-		    lcd.setCursor(0,0);
+        // MIXING SCREEN | compiles and sends CMYK color data to slave arduino for dispensing
+		lcd.setCursor(0,0);
       	lcd.print("MIX: "); lcd.print(currHex);
-      	
-      	// there will be a prepMix() function that will
-      	// handle manual entry of mixing time
-      	// this will feed into the larger mix() function
-      	// taken care of by the mixer subsystem
       	
       	lcd.setCursor(0,1);
       	lcd.print("(EXT)       BACK");
+		// (ext) means the mixer has its own standalone button and operation
       
       	currScreen = getCurrScreen(4);
         break;
 
       case 5:
-        // MIXING PROCESS SCREEN
-		    //Serial.print("Mixing process screen code start ");
-      	//Serial.println(currScreen);
-      
-      	lcd.setCursor(0,0);
-      	lcd.print("TIMER TICKS..");
-      	
-      	// there will be a timer function that handles the
-      	// counting on this screen and stops when done
-      	
-      	lcd.setCursor(0,1);
-      	lcd.print("STOP CON BACK");
-      
+        // MIXING PROCESS SCREEN | DEPRECATED
       	currScreen = getCurrScreen(5);
         break;
 
       case 6:
-        // SETTINGS SCREEN
-      	//Serial.print("Settings screen code start ");
-      	//Serial.println(currScreen);
-      
-		    lcd.setCursor(0,0);
+        // SETTINGS SCREEN | organized so that future settings can be changed
+		lcd.setCursor(0,0);
       	lcd.print("SETTINGS");
-      	
-      	// there will be options to change settings on this screen
-      	
+      	     	
       	lcd.setCursor(0,1);
       	lcd.print("RES         BACK");
       
       	currScreen = getCurrScreen(6);
         break;
-
   	}
 }
 // end of void loop()
 
-
-
-
-
-
 // UI/DISPLAY SUBSYSTEM_____________________________________________________________________________________________________________________________
+
+// main interface function
+// handles button inputs and button debounce, calls processing functions
+// returns the appropriate screen number to currScreen
 int getCurrScreen(int prevScreen)
 {
-  // handles button inputs and button debounce
-  // returns the appropriate screen number to currScreen
-
  	if (prevScreen == 0)
     {
       	button0 = digitalRead(button0Pin);
@@ -286,10 +227,6 @@ int getCurrScreen(int prevScreen)
     }
   	if (prevScreen == 2)
     {
-      	//button2 = digitalRead(button2Pin);
-      	//if (!button2) { while(!digitalRead(button2Pin)); lcd.clear(); return 3; }
-      	//button3 = digitalRead(button3Pin);
-      	//if (!button3) { while(!digitalRead(button3Pin)); lcd.clear(); return 0; }
         lcd.clear(); return 0;
     }
   	if (prevScreen == 3)
@@ -320,19 +257,17 @@ int getCurrScreen(int prevScreen)
       	if (!button0) { while(!digitalRead(button0Pin)); updateDataToSend(0); transmitData(); return 6; }
       	button3 = digitalRead(button3Pin);
       	if (!button3) { while(!digitalRead(button3Pin)); lcd.clear(); return 0; }
-    }
-  	
+    }  	
   	return prevScreen;
 }
 
-
+// iterates through the hexadecimal alphabet and displays the current character
 void scrollHex()
 {
-  //hexScrollCount = 
-
   char currLetter = currHex[currHexPos];
 
-  for (int i = 0; i < 16; i++) {
+  for (int i = 0; i < 16; i++)
+  {
     if (hexAlphabet[i] == currLetter)
     {
       if (i == 15)
@@ -351,7 +286,7 @@ void scrollHex()
   }
 }
 
-
+// iterates through each character of the currHex variable
 void nextHexPos()
 {
   if (currHexPos == 5)
@@ -365,14 +300,14 @@ void nextHexPos()
     return;
   }
 }
-
 // end ui/display functions
 
-
-
-
 // COLOR SENSOR SUBSYSTEM_____________________________________________________________________________________________________________________________
-void colorSensor() {
+
+// captures RGB color data from sensor, calculates lux and color temperature
+// calls rgbToHEX() to convert data to hexadecimal for display
+void colorSensor()
+{
   tcs.getRGBC(&red, &green, &blue, &clear);
   colorTemp = tcs.calculateColortemperature(red, green, blue);
   lux = tcs.calculateLux(red, green, blue);
@@ -386,29 +321,32 @@ void colorSensor() {
   Serial.println("\t");
 
   rgbToHEX(red, green, blue, clear, lux, currHex);
-
 }
 
-
-void rgbToHEX(uint16_t Red, uint16_t Green, uint16_t Blue, uint16_t Clear, uint16_t Lux, char currHex[8]) {
+// receives and processes RGB color data for accuracy
+// then coverts that data to a hexadecimal string currHex
+void rgbToHEX(uint16_t Red, uint16_t Green, uint16_t Blue, uint16_t Clear, uint16_t Lux, char currHex[8])
+{
   float brightness;
-  if ((Lux == 65535) || (Lux < 20)) {
+  if ((Lux == 65535) || (Lux < 20))
+  {
     brightness = 0;
   }
-  else if (Lux > 625) {
+  else if (Lux > 625)
+  {
     brightness = 5.2;
     rFact = 1800;
   }
-  else if (Lux > 300) {
+  else if (Lux > 300)
+  {
     brightness = log((float)(Lux - 20)) / 1.75;
   }
-  else {
+  else
+  {
     brightness = log((float)(Lux - 25)) / 2.5;
     bFact = 700;
   }
   
-  
-
   // Convert raw TCS34725 readings into 0–255 RGB
   if (Clear == 0)  return; 
   
@@ -417,12 +355,10 @@ void rgbToHEX(uint16_t Red, uint16_t Green, uint16_t Blue, uint16_t Clear, uint1
   g = ((uint32_t)Green * 255) / ((uint32_t)Clear * 1.0);
   b = ((uint32_t)Blue * 255) / ((uint32_t)Clear * 1.0);
   
-
   // map to proper 8-bit RGB range + adjust values to compensate for sensor error
   r = (uint8_t)constrain((r*rFact*brightness) / 1700, 0, 255);
   g = (uint8_t)constrain((g*gFact*brightness) / 1700, 0, 255);
   b = (uint8_t)constrain((b*bFact*brightness) / 1700, 0, 255);
-
 
   Serial.println();
   Serial.println("Adjusted + Mapped RGB Normalized");
@@ -431,64 +367,69 @@ void rgbToHEX(uint16_t Red, uint16_t Green, uint16_t Blue, uint16_t Clear, uint1
   Serial.print("\tB:\t"); Serial.print(b, 4);
   Serial.println("\t");
 
-
   // save 8 bit values to a singular hexadecimal variable
   sprintf(currHex, "%02X%02X%02X", (uint8_t)r, (uint8_t)g, (uint8_t)b);
 
   Serial.println();
   Serial.print("currHex: "); Serial.println(currHex);
   return;
-
 }
 
-
-void incrementLux(int incType) {
+// quick button operation to increase or decrease brightness on the scanning screen
+void incrementLux(int incType)
+{
   if (incType == 0) { luxCount = 0; return; }
 
-  if (incType == 1) {
+  if (incType == 1)
+  {
     luxCount += 1;
     Serial.print("Previous Lux: "); Serial.print(lux); Serial.print("\tLux Counter: "); Serial.println(luxCount);
     rgbToHEX(red, green, blue, clear, lux + luxCount, currHex);
     return;
   }
-  else {
+  else
+  {
     luxCount -= 1;
     Serial.print("Current Lux: "); Serial.print(lux); Serial.print("\tLux Counter: "); Serial.println(luxCount);
     rgbToHEX(red, green, blue, clear, lux + luxCount, currHex);
     return;
   }
-
 }
 // end color sensor functions
 
-
-
-
 // SYRINGES SUBSYSTEM_____________________________________________________________________________________________________________________________
-void rgbToCMYK() {
+
+// converts current, post-processing RGB color data to CMYK to be sent to the slave Arduino
+void rgbToCMYK()
+{
   float rf = r / 255.0;
   float gf = g / 255.0;
   float bf = b / 255.0;
 
   K = 1.0 - max(rf, max(gf, bf));
 
-  if (K == 1.0) {
+  if (K == 1.0)
+  {
     C = M = Y = 0.0;
-  } else {
+  } 
+  else
+  {
     C = (1.0 - rf - K) / (1.0 - K);
     M = (1.0 - gf - K) / (1.0 - K);
     Y = (1.0 - bf - K) / (1.0 - K);
   }
 }
 
-void transmitData() {
-
-    if (newStepData == true) {
+// transmits CMYK color data to the slave Arduino over I2C
+void transmitData()
+{
+    if (newStepData == true)
+	{
         Wire.beginTransmission(otherAddress);
         Wire.write((byte*) &StepData, sizeof(StepData));
         Wire.endTransmission();    // this is what actually sends the data
 
-            // for demo show the data that as been sent
+        // show the data that as been sent
         Serial.println("Sent:");
         Serial.println(StepData.C);
         Serial.println(StepData.M);
@@ -499,9 +440,12 @@ void transmitData() {
     }
 }
 
-void updateDataToSend(int mode) {
-
-    if (millis() - prevUpdateTime >= updateInterval) {
+// in mode 0, resets DataStruct to 0
+// in mode 1, calls rgbToCMYK() and updates DataStruct with results for transmission
+void updateDataToSend(int mode)
+{
+    if (millis() - prevUpdateTime >= updateInterval) // keeps the I2C bus transmitting at a constant rate in the event of communication spam
+	{
         prevUpdateTime = millis();
         if (mode == 0)
         {
@@ -511,11 +455,8 @@ void updateDataToSend(int mode) {
             StepData.Y = (uint8_t)(0);
             StepData.K = (uint8_t)(0);
         }
-        else if (newStepData == false) { // ensure previous message has been sent
-            // new data to send goes here
-
-
-            // ________________________________________________________________________________________________________________________________________!!!!!!
+        else if (newStepData == false) // ensure previous message has been sent
+		{ 
             // CMYK data values go HERE to send to slave Arduino
             rgbToCMYK();
             Serial.print("C ==== ");
@@ -532,39 +473,16 @@ void updateDataToSend(int mode) {
             StepData.Y = (uint8_t)(Y * 100);
             StepData.K = (uint8_t)(K * 100);
             
-
-
             newStepData = true;
         }
     }
 }
-
-// MIXER SUBSYSTEM_____________________________________________________________________________________________________________________________
-void prepMix()
-{
-  // handles manual entry of mixing time
-
-}
-
-void mix()
-{
-  // handles physical mixing process, and stopping and starting the mixer
-  // works with the timer() function
-
-}
-
-void timer()
-{
-  // encapsulates the timer
-
-}
-// end mixer functions
-
-
+// end syringe dispensing functions
 
 //_____________________________________________________________________________________________________________________________________________
 // for testing: how much free ram do we have?
-int freeRam() {
+int freeRam()
+{
     extern int __heap_start, *__brkval;
     int v;
     return (int)&v - (__brkval == 0 ? (int)&__heap_start : (int)__brkval);
